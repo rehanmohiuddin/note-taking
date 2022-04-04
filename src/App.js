8ba63e9026ecf172ce05df756c6aa4868ac13cb1
rehanmohiuddin <rehan.4942@gmail.com>
2022-04-04 22:19:08 +0530

    Feat:PageNotFound

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Route, Routes } from "react-router-dom";
 import Home from "./Frontend/Pages/Home";
 import Archive from "./Frontend/Pages/Archive";
 import HomePage from "./Frontend/Pages/HomePage";
+import RouteNotFound from "./Frontend/Pages/PageNotFound";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route path="/" element={<HomePage />} />
         <Route path="/tasks" element={<Home />} />
         <Route path="/archive" element={<Archive />} />
+        <Route path="*" element={<RouteNotFound />} />
       </Routes>
     </div>
   );
